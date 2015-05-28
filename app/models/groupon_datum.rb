b7bb8d63d5a699c8a63deef18ddfbb3a9d096fa8
Anna Bedell <awbedell@gmail.com>
2015-05-28 17:52:12 -0400

    methods moved to models

--- app/models/groupon_datum.rb
+++ app/models/groupon_datum.rb
@@ -1,2 +1,4 @@
 class GrouponDatum < ActiveRecord::Base
+
+  @client = Groupon::Client.new(api_key: ENV["GROUPON_KEY"])
 end
--- app/models/yelp_datum.rb
+++ app/models/yelp_datum.rb
@@ -1,2 +1,16 @@
 class YelpDatum < ActiveRecord::Base
+
+  def initialize
+    @client = Yelp::Client.new( consumer_key: ENV["YELP_CONSUMER_KEY"],
+                            consumer_secret: ENV["YELP_CONSUMER_SECRET"],
+                            token: ENV["YELP_TOKEN"],
+                            token_secret: ENV["YELP_TOKEN_SECRET"]
+                          )
+  end
+
+  def search_yelp(place)
+    @client.search(place)
+  end
+
+
 end

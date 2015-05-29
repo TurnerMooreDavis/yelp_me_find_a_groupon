a46d256282edf792374cafd8473d34a2bea4c188
TurnerMooreDavis <turnermdavis@gmail.com>
2015-05-29 12:59:18 -0400

    add yelp test

--- test/models/yelp_datum_test.rb
+++ test/models/yelp_datum_test.rb
@@ -4,4 +4,10 @@ class YelpDatumTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "yelp returns data" do
+    yelp_data = YelpDatum.new
+    p yelp_data
+    assert yelp_data
+  end
 end

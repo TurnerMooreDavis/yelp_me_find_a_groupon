60babc713346a7b34547c847758663c7eb8511ce
Anna Bedell <awbedell@gmail.com>
2015-05-30 11:26:23 -0400

    yelp tests

--- app/controllers/merge_controller.rb
+++ app/controllers/merge_controller.rb
@@ -1,5 +1,7 @@
 class MergeController < ApplicationController
 
-
+  def show
+    render json: Merge.new(params[:location])
+  end
 
 end
--- test/models/yelp_datum_test.rb
+++ test/models/yelp_datum_test.rb
@@ -8,17 +8,35 @@ class YelpDatum
 end
 
 class YelpDatumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @yelp_data = YelpDatum.new("Durham, NC")
+  end
 
   test "yelp returns data" do
-    yelp_data = YelpDatum.new("Durham, NC")
-    assert yelp_data
+    assert @yelp_data
   end
 
   test "get merchant" do
-    yelp_data = YelpDatum.new("Durham, NC")
-    assert_equal "Fullsteam Brewery", yelp_data.merchant(0)
+    assert_equal "Fullsteam Brewery", @yelp_data.merchant(0)
+  end
+
+  test "get rating" do
+    assert_equal 4.5, @yelp_data.rating(2)
+    assert_equal 4, @yelp_data.rating(3)
+  end
+
+  test "get url" do
+    assert_equal "http://www.yelp.com/biz/cocoa-cinnamon-durham", @yelp_data.url(1)
+    assert_equal "http://www.yelp.com/biz/fullsteam-brewery-durham", @yelp_data.url(0)
+  end
+
+  test "get review count" do
+    assert_equal 279, @yelp_data.review_count(0)
+    assert_equal 174, @yelp_data.review_count(1)
+  end
+
+  test "get review info" do
+    assert_equal Hash, @yelp_data.review_info(0).class
   end
 end
